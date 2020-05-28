a4b8f50e4a08cfdc2b0040ce90abd2dd3b79d2f5
abobakr99 <bamangar@gmail.com>
2020-05-28 14:06:55 -0400

    created Log in and Log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
-  
+  # login 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: "sessions#destroy"
 end
