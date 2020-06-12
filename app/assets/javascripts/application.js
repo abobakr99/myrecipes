fbf63ac90292494d94f506ee5b58f45041ab0f5d
abobakr99 <bamangar@gmail.com>
2020-06-12 15:15:36 -0400

    added chat romm feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+function scrollToBottom(){
+    if($('#messages').length > 0) {
+      $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+  }
+  
+  $(document).ready(function() {
+    scrollToBottom();
+  });
