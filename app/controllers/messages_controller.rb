b935a78e11b625eee423aeab7b58bb2b42e8af3a
abobakr99 <bamangar@gmail.com>
2020-06-13 14:08:35 -0400

    complete chat featrue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
     before_action :require_user
-  
+
     def create
       @message = Message.new(message_params)
       @message.chef = current_chef
